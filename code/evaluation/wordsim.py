f0cedc7e5d709bc2540768fd76c864d788d22070
Joshua Chin <jchin@luminoso.com>
2015-07-13 14:29:53 -0400

    cleaned up evaluation code

--- code/evaluation/wordsim.py
+++ code/evaluation/wordsim.py
@@ -10,35 +10,37 @@ def evaluate(similarity_func, standard):
 
 def test_all(similarity_func):
     print("wordsim-353")
-    print(evaluate(assoc, parse_wordsim()))
+    print(evaluate(similarity_func, parse_wordsim()))
     print("men-3000")
-    print(evaluate(assoc, parse_men3000()))
+    print(evaluate(similarity_func, parse_men3000()))
     print("rg65")
-    print(evaluate(assoc, parse_rg()))
+    print(evaluate(similarity_func, parse_rg()))
     print("rw")
-    print(evaluate(assoc, parse_rw()))
+    print(evaluate(similarity_func, parse_rw()))
+
+def parse_file(filename, sep=None, preprocess_word=None):
+    with open(filename) as file:
+        for line in file:
+            if sep is None:
+                w1, w2, val = line.strip().split()
+            else:
+                w1, w2, val = line.strip().split(sep)
+
+            if preprocess_word is not None:
+                w1 = preprocess_word(w1)
+                w2 = preprocess_word(w2)
+
+            yield w1, w2, float(val)
+
 
 def parse_wordsim(filename='data/ws353.csv'):
-    with open(filename) as file:
-        next(file) # skip header
-        for line in file:
-            w1, w2, val = line.strip().split(',')
-            yield w1, w2, float(val)
+    return parse_file(filename, sep=',')
 
 def parse_men3000(filename='data/men3000.csv'):
-    with open(filename) as file:
-        for line in file:
-            w1, w2, val = line.strip().split()
-            yield w1.split('-')[0], w2.split('-')[0], float(val)
+    return parse_file(filename, preprocess_word=lambda w: w.split('-')[0])
 
 def parse_rw(filename='data/rw.csv'):
-    with open(filename) as file:
-        for line in file:
-            w1, w2, avg, *_ = line.split()
-            yield w1, w2, float(avg)
+    return parse_file(filename)
 
 def parse_rg65(filename='data/rg-65.csv'):
-    with open(filename) as file:
-        for line in file:
-            w1, w2, val = line.strip().split()
-            yield w1, w2, float(val)
+    return parse_file(filename)
