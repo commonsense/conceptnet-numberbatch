e782c35c1cc1c6289346d2a7f17014e6d4605cad
Joshua Chin <jchin@luminoso.com>
2015-07-14 10:34:45 -0400

    updated gitignore

---
+++ code/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+
+setup(
+    name = 'ConceptNet Retrofitting Paper',
+    version = '0.0'
+)
