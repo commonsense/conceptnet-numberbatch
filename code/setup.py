8c4c2ae2c78ab3b916a52021cbe835075d1b1c3b
Rob Speer <rob@luminoso.com>
2015-07-16 15:28:14 -0400

    declare dependencies

--- code/setup.py
+++ code/setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup
-from setuptools.command.install import install
-from setuptools.command.develop import develop
 
 setup(
     name = 'ConceptNet Retrofitting Paper',
-    version = '0.0'
+    version = '0.0',
+    install_requires=['pandas', 'conceptnet', 'ftfy']
 )
