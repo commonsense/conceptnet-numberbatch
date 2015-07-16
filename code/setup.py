6c70e38583381ee405ec627010c2835cc5ae127c
Joshua Chin <jchin@luminoso.com>
2015-07-16 16:48:31 -0400

    added indentation

--- code/setup.py
+++ code/setup.py
@@ -4,6 +4,6 @@ setup(
     name = 'ConceptNet Retrofitting Paper',
     version = '0.1',
     install_requires=[
-    'pandas', 'conceptnet==5.4a1', 'ftfy>=4.0', 'numpy', 'scikit-learn'
+        'pandas', 'conceptnet==5.4a1', 'ftfy>=4.0', 'numpy', 'scikit-learn'
     ]
 )
