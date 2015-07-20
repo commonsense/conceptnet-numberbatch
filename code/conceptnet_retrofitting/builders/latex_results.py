4ad52b14acfbe1bfc4d1ff1bb9cfee260a930e76
Joshua Chin <jchin@luminoso.com>
2015-07-20 12:45:14 -0400

    fixed latex table order

--- code/conceptnet_retrofitting/builders/latex_results.py
+++ code/conceptnet_retrofitting/builders/latex_results.py
@@ -2,7 +2,7 @@ from collections import OrderedDict, defaultdict
 import pandas as pd
 
 def main(in_files, out_file):
-    in_files.sort(key=to_simple_name)
+    in_files.sort(key=order_names)
 
     data = OrderedDict((k, []) for k in parse_evaluation(in_files[0]))
     vectors = [to_simple_name(filename) for filename in in_files]
@@ -47,6 +47,22 @@ def to_simple_name(filename):
         simple_name.append('(l2)')
 
     return ' '.join(simple_name)
+
+def order_names(filename):
+    out = [0, 0]
+    simple = to_simple_name(filename)
+
+    if simple.startswith('retrofitted'):
+        out[0] = 2
+    elif simple.startswith('standardized'):
+        out[0] = 1
+
+    if simple.endswith('(l1)'):
+        out[1] = 2
+    elif simple.endswith('(l2)'):
+        out[1] = 1
+
+    return out
 
 if __name__ == '__main__':
     import sys
