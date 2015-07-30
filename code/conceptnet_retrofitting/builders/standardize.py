1e2139d84c26e0c25d4951c4e5b72b8683ffc7a1
Joshua Chin <jchin@luminoso.com>
2015-07-30 14:36:13 -0400

    changed flag name

--- code/conceptnet_retrofitting/builders/standardize.py
+++ code/conceptnet_retrofitting/builders/standardize.py
@@ -1,7 +1,7 @@
 from unicodedata import normalize
 from conceptnet5.nodes import standardized_concept_uri
 
-def standardize(text, lang='en', NFD_normalize=True):
+def standardize(text, lang='en', remove_accents=True):
     if NFD_normalize and (lang=='es' or text.startswith('/c/es/')):
         text = normalize('NFD', text).encode('ascii', errors='ignore').decode()
     if not text.startswith('/c/'):
